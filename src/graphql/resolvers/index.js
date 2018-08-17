0ecd6162e55fbfe70012b7ca9f14a7301a1f32b3
Arol <arolet@gmail.com>
2018-08-17 10:40:35 +0200

    refactor: referencing fufunctions directly

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -6,24 +6,24 @@ const world = require('./world-cup.resolver');
 
 const resolvers = {
   Query: {
-    team: () => query.team(),
-    stadium: () => query.stadium(),
-    channels: () => query.channels(),
-    matches: (_, args) => query.matches(_, args),
-    group: (_, { name }) => query.group(_, { name }),
-    knockout: (_, { name }) => query.knockout(_, { name })
+    team: query.team,
+    stadium: query.stadium,
+    channels: query.channels,
+    matches: query.matches,
+    group: query.group,
+    knockout: query.knockout
   },
   Mutation: {
-    updateMatch: (_, args) => mutation.updateMatch(_, args)
+    updateMatch: mutation.updateMatch
   },
   Match: {
-    home_team: (match) => world.get_home_team(match),
-    away_team: (match) => world.get_away_team(match),
-    stadium: (match) => world.get_stadium(match),
-    channels: (match) => world.get_channels(match),
+    home_team: world.get_home_team,
+    away_team: world.get_away_team,
+    stadium: world.get_stadium,
+    channels: world.get_channels,
   },
   Stadium: {
-    lastPlayed: (stadium) => world.get_lastPlayed(stadium)
+    lastPlayed: world.get_lastPlayed
   }
 };
 
